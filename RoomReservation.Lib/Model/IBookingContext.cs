300f2362a74e0be2de50f2bd6efdbb4093886463
akazemis <akazemis@gmail.com>
2016-10-16 13:07:54 +1100

    IBookingContext tweaks

--- RoomReservation.Lib/Model/IBookingContext.cs
+++ RoomReservation.Lib/Model/IBookingContext.cs
@@ -1,11 +1,14 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 
 namespace RoomReservation.Lib.Model
 {
-    public interface IBookingContext
+    public interface IBookingContext : IDisposable
     {
         DbSet<Room> Rooms { get; set; }
         DbSet<Person> People { get; set; }
         DbSet<BookingSchedule> BookingSchedules { get; set; }
+
+        int SaveChanges();
     }
 }
